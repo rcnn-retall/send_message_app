ffd33417ad5535a8220dc42c06202119dbf325c7
rcnn-retall <62229735+rcnn-retall@users.noreply.github.com>
2025-01-10 16:00:45 +0800

    123

--- client_main/userinfo/Userinfo.py
+++ client_main/userinfo/Userinfo.py
@@ -1,6 +1,7 @@
 from kivy.uix.screenmanager import Screen
 # from kivy.uix.button import Button
-from utils.uix import U_button
+from utils.uix import U_button, U_clear_color_Button
+from kivy.metrics import sp
 
 name = "User"
 
@@ -8,6 +9,8 @@ class View(Screen):
     def __init__(self, **kwargs):
         super(View, self).__init__(**kwargs)
         self.name = name
-        self.add_widget(U_button(text="菜单栏"))
+        self.add_widget(U_clear_color_Button(text="用户登入", font_size=sp(20), size_hint=(.5,.075), pos_hint={"top":1,"left":1}))
+        self.add_widget(U_clear_color_Button(text="用户注册", font_size=sp(20), size_hint=(.5,.075),pos_hint={"top":1,"right":1}))
+
 
 
--- client_main/utils/uix.py
+++ client_main/utils/uix.py
@@ -1,6 +1,7 @@
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.utils import get_color_from_hex
+from kivy.graphics import Rectangle, Color,
 
 
 class U_wiget(object):
@@ -20,5 +21,10 @@ class U_button(Button, U_wiget):
         for key, value in kwargs.items():
             self.__setattr__(key, value)
 
-
+class U_clear_color_Button(Button, U_wiget):
+    def __init__(self,**kwargs):
+        super(U_clear_color_Button, self).__init__()
+        for key, value in kwargs.items():
+            self.__setattr__(key,value)
+        self.background_color = (1,1,1,0)
 
